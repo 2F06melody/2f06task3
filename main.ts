b73d862cc6850a6d639fe42d5cf191438a06b311
2F06melody <115759014+2F06melody@users.noreply.github.com>
2022-10-14 09:30:03 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,36 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(randint(0, 10))
+    hi = 0
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(9)
-    basic.pause(1000)
-    basic.showNumber(8)
-    basic.pause(1000)
-    basic.showNumber(7)
-    basic.pause(1000)
-    basic.showNumber(6)
-    basic.pause(1000)
-    basic.showNumber(5)
-    basic.pause(1000)
-    basic.showNumber(4)
-    basic.pause(1000)
-    basic.showNumber(3)
-    basic.pause(1000)
-    basic.showNumber(2)
-    basic.pause(1000)
-    basic.showNumber(1)
-    basic.pause(1000)
+    hi = 9
+    while (hi >= 0) {
+        basic.showNumber(hi)
+        hi += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
 })
+let hi = 0
+basic.showString("2F06 chaumut")
 basic.forever(function () {
 	
 })
